4b5f73bd4c7043d543f3fbc43cbd147ed0571967
Meelenych <meelenych@gmail.com>
2021-11-23 19:49:40 +0200

    mail files added

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+const Filter = ({filter}) => {
+  
+    return (
+        <>
+            <label>Find contacts by name
+                <input                   
+                    type="text"
+                    onChange={filter}                
+                />
+            </label>
+        </>
+    )
+
+}
+
+
+
+export default Filter
